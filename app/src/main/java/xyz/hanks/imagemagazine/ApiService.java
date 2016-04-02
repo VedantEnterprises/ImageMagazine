f9a002e575f17c729d7b353e60aef0c39d7411b5
hanks-zyh <zhangyuhan2014@gmail.com>
2016-04-02 20:22:58 +0800

    get subscribed list data

--- app/src/main/java/xyz/hanks/imagemagazine/ApiService.java
+++ app/src/main/java/xyz/hanks/imagemagazine/ApiService.java
@@ -1,6 +1,9 @@
 package xyz.hanks.imagemagazine;
 
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import rx.Observable;
 
 /**
@@ -8,10 +11,17 @@ import rx.Observable;
  */
 public interface ApiService {
 
-    @GET("magazine/api/magazine/list")
-    Observable<Result> getList();
+    @GET("magazine/api/magazine/list") Observable<Result> getList();
 
 
-    @GET("magazine/api/magazine/extraData")
-    Observable<Result> getExtraData();
+    @GET("magazine/api/magazine/extraData") Observable<Result> getExtraData();
+
+
+    @FormUrlEncoded
+    @POST("magazine/api/periodical/listSubscribe") Observable<Result> listSubscribe(@Field(
+            "magazineIds") String magazineIds, @Field("size") String size);
+
+//    http://appstatic.lenovomm.com/static/magazine/20160330/8746cf0bbe24411598125f37f49fbac1
+
 }
+
